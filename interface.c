01cb4bf01fccfe1dc04632a0e493efa1896a985a
André Santos <andrerfcsantos@gmail.com>
2015-03-13 02:40:01 +0000

    Pequenas mudanças ao main, interface funcional - Pequenas mudanças no main - ver diff. - Interface 23.99% mais funcional.

--- interface.c
+++ interface.c
@@ -13,52 +13,59 @@
 #define BLUE  "\x1B[34m"
 #define WHITE  "\x1B[37m"
 
-void faceprodutos();
-void faceclientes();
-void facecompras();
+int faceprodutos();
+int faceclientes();
+int facecompras();
 
 void interface() {
-    char *r;
-    int sair = 0, leitura;
-    printf("%s ------------------------------------------\n|"
-            "%sGESTHIPER%s                                 |\n|"
-            "                                          |\n|"
-            "   1 - Produtos                           |\n|"
-            "   2 - Clientes                           |\n|"
-            "   3 - Compras/Contabilidade              |\n|"
-            "                                          |\n| "
-            "BEM-VINDO                       Q - %sSair%s |\n "
-            "------------------------------------------\n%s",
-            WHITE, BLUE, WHITE, RED, WHITE, NORMAL);
+    char r[50];
+    int sair_programa = 0, leitura;
+    
+    printf("Pressione qualquer tecla para continuar: ");
+    leitura = scanf("%s", r);
+    
+    while (sair_programa == 0) {
+        printf("\033[2J\033[1;1H%s ------------------------------------------\n|"
+                "%sGESTHIPER%s                                 |\n|"
+                "                                          |\n|"
+                "   1 - Produtos                           |\n|"
+                "   2 - Clientes                           |\n|"
+                "   3 - Compras/Contabilidade              |\n|"
+                "                                          |\n| "
+                "BEM-VINDO                       Q - %sSair%s |\n "
+                "------------------------------------------\n%s",
+                WHITE, BLUE, WHITE, RED, WHITE, NORMAL);
 
 
-    while (sair == 0) {
-        r = (char *) malloc(sizeof(char)*50);
         printf("//O que procura(?): ");
-        leitura=scanf("%s\n", r);
-        
+        leitura = scanf("%s", r);
+
         switch (toupper(r[0])) {
-            case '1': faceprodutos();break;
-            case '2': faceclientes();break;
-            case '3': facecompras();break;
-            case 'Q': sair = 1;break;
-            default: 
+            case '1': sair_programa = faceprodutos();
+                break;
+            case '2': sair_programa = faceclientes();
+                break;
+            case '3': sair_programa = facecompras();
+                break;
+            case 'Q': sair_programa = 1;
+                break;
+            default:
                 /*TODO: Tratamento de erro temporario.
                  * Melhorar este tratamento depois de saber resposta do professor
                  sobre o uso do scanf.*/
-                if(leitura==0) error_msg(ERRO_NENHUMA_OPCAO, NULL);
+                if (leitura == 0) error_msg(ERRO_NENHUMA_OPCAO, NULL);
                 else error_msg(ERRO_OPCAO_INVALIDA, NULL);
                 break;
         }
-        free(r);
     }
 
 
 }
 
-void faceprodutos() {
-    char *r;
-    int sair=0, leitura;
+int faceprodutos() {
+    char r[50];
+    int sair_menu=0, leitura;
+    int sair_programa=0;
     
     printf("\033[2J\033[1;1H%s ------------------------------------------\n|"
             "%s*1* %sPRODUTOS%s                              |\n|"
@@ -72,17 +79,20 @@ void faceprodutos() {
             WHITE, YELLOW, BLUE, WHITE, RED, WHITE, NORMAL);
     
     
-    while (sair == 0) {
-        r = (char *) malloc(sizeof(char)*50);
+    while (sair_menu == 0) {
         printf("//O que procura(?): ");
-        leitura = scanf("%s\n", r);
+        leitura = scanf("%s", r);
         switch (toupper(r[0])) {
             case '0': printf("\033[2J\033[1;1H");
-                      interface();break;
+                      sair_menu=1;
+                      sair_programa=0;
+                      break;
             case '1': _02_codigo_produtos_letra(); break;
             case '2': _04_prods_nao_comprados(); break;
             case '3': _08_clientes_compraram_prod(); break;
-            case 'Q': sair = 1;break;
+            case 'Q': sair_menu = 1;
+                      sair_programa = 1;
+                      break;
             default: 
                 /*TODO: Tratamento de erro temporario.
                  * Melhorar este tratamento depois de saber resposta do professor
@@ -91,14 +101,15 @@ void faceprodutos() {
                 else error_msg(ERRO_OPCAO_INVALIDA, NULL);
                 break;
         }
-        free(r);
     }
-    
+    return sair_programa;
 }
 
-void faceclientes() {
+int faceclientes() {
     char r[50];
-    int sair = 0, leitura;
+    int sair_menu = 0, sair_programa=0;
+    int leitura;
+    
     printf("\033[2J\033[1;1H%s ------------------------------------------\n|"
             "%s*2* %sCLIENTES%s                              |\n|"
             "                                                      |\n|"
@@ -111,20 +122,24 @@ void faceclientes() {
     printf("|                                          |\n|"
             "  0 - Voltar                     Q - %sSair%s |\n"
             " ------------------------------------------\n%s", RED, WHITE, NORMAL);
-        while (sair == 0) {
+        while (sair_menu == 0) {
 
         printf("//O que procura(?): ");
-        leitura = scanf("%s\n", r);
+        leitura = scanf("%s", r);
         
         switch (toupper(r[0])) {
             case '0': printf("\033[2J\033[1;1H");
-                      interface();break;
+                      sair_menu=1;
+                      sair_programa=0;
+                      break;
             case '1': _05_tabela_cliente(); break;
             case '2': _06_codigos_clientes_letra(); break;
             case '3': _09_produtos_mais_comprados_cliente(); break;
             case '4': _10_clientes_regulares(); break;
             case '5': _13_tres_prods_mais_comprados(); break;
-            case 'Q': sair = 1;break;
+            case 'Q': sair_menu = 1;
+                      sair_programa=1;
+                      break;
             default: 
                 /*TODO: Tratamento de erro temporario.
                  * Melhorar este tratamento depois de saber resposta do professor
@@ -134,12 +149,13 @@ void faceclientes() {
                 break;
         }
     }
-
+    return sair_programa;
 }
 
-void facecompras() {
+int facecompras() {
     char r[50];
-    int sair = 0, leitura;
+    int sair_menu = 0,sair_programa=0;
+    int leitura;
     printf("\033[2J\033[1;1H%s ------------------------------------------\n|"
             "%s*3* %sCONTABILIDADE%s                         |\n|"
             "                                                |\n|"
@@ -153,20 +169,24 @@ void facecompras() {
             "  0 - Voltar                     Q - %sSair%s |\n"
             " ------------------------------------------\n%s", RED, WHITE, NORMAL);
 
-    while (sair == 0) {
+    while (sair_menu == 0) {
 
         printf("//O que procura(?): ");
-        leitura = scanf("%s\n", r);
+        leitura = scanf("%s", r);
 
         switch (toupper(r[0])) {
             case '0': printf("\033[2J\033[1;1H");
-                interface();break;
+                      sair_menu=1;
+                      sair_programa=0;
+                      break;
             case '1': _03_compras_mensais_prod();break;
             case '2': _07_compras_intervalo_meses();break;
             case '3': _11_compras_CSV();break;
             case '4': _12_prods_mais_vendidos();break;
             case '5': _14_clientes_prods_fantasma();break;
-            case 'Q': sair = 1;break;
+            case 'Q': sair_menu = 1;
+                      sair_programa = 1;
+                      break;
             default:
                 /*TODO: Tratamento de erro temporario.
                  * Melhorar este tratamento depois de saber resposta do professor
@@ -176,6 +196,5 @@ void facecompras() {
                 break;
         }
     }
-
-
+    return sair_programa;
 }
--- main.c
+++ main.c
@@ -16,8 +16,6 @@ int le_ficheiros(int, char **);
 void le_clientes(FILE *, char *);
 void le_produtos(FILE *, char *);
 void le_compras(FILE *, char *);
-int cliente_valido(char *);
-int produto_valido(char *);
 int compra_valida(COMPRA *);
 void mostra_compra(COMPRA *);
 void imprime_codigos_comecados_por_letra(CATALOGO, char);
@@ -31,10 +29,7 @@ int main(int argc, char** argv) {
     
     le_ficheiros(argc, argv);
     
-    if(procura_elemento(catalogo_clientes,"AH265")!=NULL)
-        printf("O elemento foi encontrado.\n");
-    
-    /* interface(); */
+    interface();
     return (EXIT_SUCCESS);
 }
 
@@ -92,30 +87,26 @@ void le_clientes(FILE *f_cli, char *nf) {
     int clientes_validos = 0, total_linhas_clientes = 0;
     clock_t ci, cf;
     char *cliente, *linha_cliente;
-    char *delim = "\n\r";
-    
+    char *delim = " \n\r";
+
     ci = clock();
-    linha_cliente = (char *) malloc(sizeof (char)*LINHA_CLIENTE_MAX);
-    
+    linha_cliente = (char *) malloc(sizeof(char)*LINHA_CLIENTE_MAX);
+
     while (fgets(linha_cliente, LINHA_CLIENTE_MAX, f_cli) != NULL) {
         cliente = strtok(linha_cliente, delim);
-
-        if (cliente_valido(cliente) != -1) {
-            insere_item(catalogo_clientes, cliente);
-            clientes_validos++;
-        }
-
+        insere_item(catalogo_clientes, cliente);
+        clientes_validos++;
         total_linhas_clientes++;
     }
-    
+
     cf = clock();
-    
+
     printf("-----CLIENTES-----\n");
     printf("Nome do ficheiro: %s\n", nf);
     printf("Linhas validas: %d\n", clientes_validos);
     printf("Linhas lidas: %d\n", total_linhas_clientes);
     printf("Leitura em %f segundos.\n", ((float) cf - ci) / CLOCKS_PER_SEC);
-    
+
     free(linha_cliente);
 }
 
@@ -123,26 +114,21 @@ void le_produtos(FILE *f_prod, char *nf) {
     int produtos_validos = 0, total_linhas_produtos = 0;
     clock_t ci, cf;
     char *produto, *linha_produto;
-    char *delim = "\n\r";
-    
+    char *delim = " \n\r";
+
     ci = clock();
-    
-    linha_produto = (char *) malloc(sizeof (char)*LINHA_PRODUTO_MAX);
-    
+
+    linha_produto = (char *) malloc(sizeof(char)*LINHA_PRODUTO_MAX);
+
     while (fgets(linha_produto, LINHA_PRODUTO_MAX, f_prod) != NULL) {
         produto = strtok(linha_produto, delim);
-        
-        if (produto_valido(produto) != -1) {
-            produtos_validos++;
-            insere_item(catalogo_produtos,produto);
-        }
-        
-        /*printf("%s\n",produto);*/
+        insere_item(catalogo_produtos, produto);
+        produtos_validos++;
         total_linhas_produtos++;
     }
-    
+
     cf = clock();
-    
+
     printf("-----PRODUTOS-----\n");
     printf("Nome do ficheiro: %s\n", nf);
     printf("Linhas validas: %d\n", produtos_validos);
@@ -152,16 +138,16 @@ void le_produtos(FILE *f_prod, char *nf) {
 }
 
 void le_compras(FILE *f_comp, char *nf) {
+    COMPRA *compra;
     int compras_validas = 0, total_linhas_compras = 0;
     clock_t ci, cf;
-    char *linha_compra, *delim, *token;
-    COMPRA *compra;
+    char *linha_compra, *token;
+    char *delim = " \n\r";
 
     ci = clock();
-    linha_compra = (char *) malloc(sizeof (char)*LINHA_COMPRA_MAX);
     
+    linha_compra = (char *) malloc(sizeof (char)*LINHA_COMPRA_MAX);
     compra = inicializa_compra();
-    delim = " \n\r";
 
     while (fgets(linha_compra, LINHA_COMPRA_MAX, f_comp) != NULL) {
         
@@ -186,7 +172,6 @@ void le_compras(FILE *f_comp, char *nf) {
 
         if (compra_valida(compra)) {
             compras_validas++;
-            /*mostra_compra(compra);*/
         }
         total_linhas_compras++;
     }
@@ -202,39 +187,12 @@ void le_compras(FILE *f_comp, char *nf) {
     free(linha_compra);
 }
 
-int cliente_valido(char *cl) {
-    int res = 0;
-    int tam = strlen(cl);
 
-    if (tam != 5) {
-        res = -1;
-    } else {
-        res = isupper(cl[0]) && isupper(cl[1]) &&
-                isdigit(cl[2]) && isdigit(cl[3]) && isdigit(cl[4]);
-    }
-
-    return res;
-}
-
-int produto_valido(char *prod) {
-    int res = 0;
-    int tam = strlen(prod);
-
-    if (tam != 6) {
-        res = -1;
-    } else {
-        res = isupper(prod[0]) && isupper(prod[1]) &&
-                isdigit(prod[2]) && isdigit(prod[3]) &&
-                isdigit(prod[4]) && isdigit(prod[5]);
-    }
-
-    return res;
-}
-
+/*
+ * TODO: Verificar se produto e cliente existem, depois de ter catalogos prontos.
+ */
 int compra_valida(COMPRA *compra) {
-    return (cliente_valido(get_cod_cliente(compra)) != -1)
-            && (produto_valido(get_cod_produto(compra)) != -1)
-            && get_mes(compra) >= 1 && get_mes(compra) <= 12
+    return get_mes(compra) >= 1 && get_mes(compra) <= 12
             && get_preco_unit(compra) >= 0
             && (get_promo(compra) == 'N' || get_promo(compra) == 'P');
 }
